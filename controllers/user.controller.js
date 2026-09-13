56e1bf4b62cddf33851f4368ac4dd20919cafcf6
mahdi zare <mohemadzare13@gamil.com>
2026-09-13 03:37:50 -0700

    fix:add ficher for userControllerAndroutes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -40,7 +40,27 @@ exports.remove = async (req, res) => {
 };
 exports.chengrole = async (req, res) => {
   const { id } = req.body;
-  const user = await UserModel.findById({ id: _id });
-  const newRole = user.role === "ADMIN" ? "USER" : "ADMIN";
+  const isValideUserID = mongoose.isValidObjectId(id);
+  if (!isValideUserID) {
+    return res.status(409).json({
+      message: "user id is not valid",
+    });
+  }
+  const user = await UserModel.findOne({ _id: id });
+  let newRole = user.role === "ADMIN" ? "USER" : "ADMIN";
   console.log(newRole);
-}; 
+  const updateUser = await UserModel.findByIdAndUpdate(
+    { _id: id },
+    {
+      role: newRole,
+    },
+  );
+  if (updateUser) {
+    return res.status(200).json({
+      message: "User Roel Changer Sussesfully",
+    });
+  }
+  return res.status(500).json({
+    message: "internal server error",
+  });
+};
