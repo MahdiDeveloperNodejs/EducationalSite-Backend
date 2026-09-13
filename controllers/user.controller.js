72b1ece7718eac081cc9a072b6f7c07f159c1bc0
mahdi zare <mohemadzare13@gamil.com>
2026-09-13 02:51:37 -0700

    feat:minor chengers in user.route user.controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const BanUserModel = require("../models/banUser.model");
 const UserModel = require("../models/user.model");
 
@@ -19,4 +20,27 @@ exports.getAll = async (req, res) => {
   const user = await UserModel.find({});
 
   return res.json(user);
+};
+exports.remove = async (req, res) => {
+  const isValideUserID = mongoose.isValidObjectId(req.params.id);
+  if (!isValideUserID) {
+    return res.status(409).json({
+      message: "user id is not vlide",
+    });
+  }
+  const removedUser = await UserModel.findByIdAndDelete({ _id: req.params.id });
+  if (!removedUser) {
+    return res.status(403).json({
+      message: "ther is no user",
+    });
+  }
+  return res.status(200).json({
+    message: "deleted successfully.",
+  });
+};
+exports.chengrole = async (req, res) => {
+  const { id } = req.body;
+  const user = await UserModel.findById({ id: _id });
+  const newRole = user.role === "ADMIN" ? "USER" : "ADMIN";
+  console.log(newRole);
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,13 +3,18 @@ const router = Router();
 const userController = require("../controllers/user.controller");
 const Authmidelwer = require("../middlewares/auth");
 const isAuthmidelwer = require("../middlewares/auth");
-
 router
   .route("/getAll")
   .get(Authmidelwer, isAuthmidelwer, userController.getAll);
 
 router.post("/Ban/:id", Authmidelwer, isAuthmidelwer, userController.banUser);
+router
+  .route("/:id")
+  .delete(Authmidelwer, isAuthmidelwer, userController.remove);
 
+router
+  .route("/role")
+  .put(Authmidelwer, isAuthmidelwer, userController.chengrole);
 module.exports = {
   UserRouter: router,
 };
