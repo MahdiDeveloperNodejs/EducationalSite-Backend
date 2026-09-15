a109d72e7efe814b2f2ca0ed8ab73e32de148cd3
mahdi zare <mohemadzare13@gmail.com>
2026-09-15 15:49:50 -0700

    fix: bog yor model comment

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,12 +1,17 @@
 const { Schema, model } = require("mongoose");
 
-const CommentSchema = new Schema({
-  body: { type: String, required: true },
-  course: { type: Schema.Types.ObjectId, ref: "course", required: true },
-  creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  isAccept: { type: Number, default: 0 },
-  score: { type: Number, default: 5 },
-});
+const CommentSchema = new Schema(
+  {
+    body: { type: String, required: true },
+    course: { type: Schema.Types.ObjectId, ref: "course", required: true },
+    creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    isAccept: { type: Number, default: 0 },
+    score: { type: Number, default: 5 },
+    isAnswer: { type: Number, required: true },
+    mainCommentId: { type: Schema.Types.ObjectId, ref: "Comment" },
+  },
+  { timestamps: true },
+);
 
 const CommentModel = model("Comment", CommentSchema);
 module.exports = CommentModel;
